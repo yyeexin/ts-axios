fecb7b2a852218918202eb3ec7ac0f8f49d794c9
yyeexin <248480904@qq.com>
2020-02-07 22:49:17 +0800

    feat: 基础请求功能开发

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+//字符串字面量类型允许你指定字符串必须具有的确切值
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
